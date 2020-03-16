2c1e1f02325e14edb1486b14c6fe24f0c176e94a
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-03-16 14:23:42 +0900

    replaced_user#show

--- app/assets/javascripts/items/show.js
+++ app/assets/javascripts/items/show.js
@@ -1,23 +1,10 @@
 $(function()  {
-  // タブのDOM要素を取得し、変数で定義
   let tabs = $(".js__li");
-
-  // クラスの切り替えをtabSwitch関数で定義
   function tabSwitch() {
-    // 全てのactiveクラスのうち、最初の要素を削除（"[0]は、最初の要素の意味"）
     $(".active").removeClass("active");
-
-    // クリックしたタブにactiveクラスを追加
-
     $(this).addClass("active");
-
-    // 何番目の要素がクリックされたかを、配列tabsから要素番号を取得
     const index = tabs.index(this);
-
-    // クリックしたタブに対応するshowクラスを追加する
     $(".li__bottom").removeClass("delete").eq(index).addClass("show");
   }
-
-  // タブがクリックされたらtabSwitch関数を呼び出す
   tabs.click(tabSwitch);    
 });
