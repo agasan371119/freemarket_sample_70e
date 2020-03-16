42dc26ebb5856b10b1eee74f7fcda9aef1838181
hyoro-minakichi <60648234+hyoro-minakichi@users.noreply.github.com>
2020-03-16 14:09:42 +0900

    3/16修正分⑤

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,6 @@ class User < ApplicationRecord
   validates :familyname_kana, :firstname_kana, presence: true, 
               format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角で入力してください" }
 
-  # validates :nickname, :firstname, :lastname, :birthday, presence: true
-  #             {message: "ニックネームを入力してください" }
-  # validates :email, presence: true, uniqueness: { case_sensitive: false }, 
-  #               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "メールアドレスを入力してください" }
-  # validates :encrypted_password, presence: true, length: { minimum: 7 }, 
-  #             format: { with: /\A[a-z0-9]+\z/i, message: "パスワードを入力してください" }
-  # validates :familyname, presence: true, {message: "姓 を入力してください" }
-  # validates :firstname, presence: true, {message: "名 を入力してください" }
-  # validates :kana_firstname, presence: true, 
-  #             format: { with: /\A([ァ-ン]|ー)+\z/, message: "姓カナ を入力してください" }
-  # validates :kana_lastname, presence: true, 
-  #             format: { with: /\A([ァ-ン]|ー)+\z/, message: "名カナ を入力してください" }
-  # validates :birth_year, :birth_month, :birth_day, presence: true, {message: "生年月日を入力してください" }
-    
   
 
   has_many :reviews
