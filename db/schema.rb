8d6014456e9cae2f466cf64702672cdcb519f252
hyoro-minakichi <60648234+hyoro-minakichi@users.noreply.github.com>
2020-03-16 14:56:19 +0900

    3/16修正⑦

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_001335) do
-
+ActiveRecord::Schema.define(version: 2020_03_12_124642) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -31,15 +30,12 @@ ActiveRecord::Schema.define(version: 2020_03_12_001335) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-ActiveRecord::Schema.define(version: 2020_03_12_124642) do
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image", null: false
