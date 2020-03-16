c53dd7e9249b3f6abbb8a971a252bd66d723536d
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-03-16 13:50:46 +0900

    create_user#show_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # , controllers: {
   #   registrations: 'users/registrations',
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   # root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :show]
   resources :items, only: [:index, :new] do
     member do
       get 'buy'
