80d81376b2025699efc35756b7ade21c333d3ac3
gonzaburoudesu <fantasy.soccer@icloud.com>
2020-03-16 21:11:42 +0900

    top-page-change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_12_124642) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -19,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_124642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image", null: false
