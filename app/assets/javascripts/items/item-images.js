b02ee28c807813f9eec6572f01fc50a809b71365
hyoro-minakichi <60648234+hyoro-minakichi@users.noreply.github.com>
2020-03-19 09:22:10 +0900

    3/17作業分

---
+++ app/assets/javascripts/items/item-images.js
@@ -0,0 +1,10 @@
+
+$(document).on('turbolinks:load', function(){
+
+  $(document).on('click', '.image_upload', function(){
+    var preview = $('<div class="image-preview__wapper"><img class="preview"></div><div class="image-preview_btn"><div class="image-preview_btn_edit">編集</div><div class="image-preview_btn_delete">削除</div></div>'); 
+    var append_input = $(`<li class="input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
+    $ul = $('#previews')
+  });
+
+}
