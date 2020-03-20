1d7f7ee6725bc0cd2dc81d5be617e1720b2a84ba
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-03-20 20:56:10 +0900

    replaced

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,7 @@ class ItemsController < ApplicationController
 
   def sold
     item = Item.find(params[:id])
-    item.save(buyer_id: current_user.id)
-    if item.save
+    if item.save(buyer_id: current_user.id)
       redirect_to root_path
   else
     flash.now[:alert] = "クレジットカードを登録して下さい"
