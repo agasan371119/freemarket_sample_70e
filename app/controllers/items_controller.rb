67ea92c7e3a530d4b0738c51e0d5ad68ad3a8116
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-03-20 18:50:32 +0900

    replaced_item#index_something_else

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,27 +3,18 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.limit(5).order("created_at DESC")
     @ladies = Item.where(category_id: 5).limit(5)
+    @mens = Item.where(category_id: 138).limit(5)
     @categories = Category.where(ancestry: nil)
+    @category_children = Category.find_by(params[:parent_name]).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
-  def category_children_index
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-  
   def show
-    @item = Item.find(1     )
-  end
-
-  def buy
     @item = Item.find(1)
-    @address = Address.find_by(user_id: current_user.id)
   end
-
-  def sold
-    item.update(buyer_id: current_user.id)
-    redirect_to root_path
-  end
-
 
   def new
     @item = Item.new
@@ -39,21 +30,26 @@ class ItemsController < ApplicationController
 
   end
 
- 
   def category_grandchildren
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
-  def create
-    
+  def create  
   end
 
+  def buy
+    @item = Item.find(17)
+    @address = Address.find_by(user_id: current_user.id)
+  end
+
+  def sold
+    item = Item.find(params[:id])
+    item.update!(buyer_id: current_user.id)
+    redirect_to root_path
+  end
 
   private
-  
   def item_params
-    params.require(:item).permit(:name,:price, buyer_id:current_user_id)
+    params.require(:item).permit(:name,:price)
   end
-
-
 end
