4ef898e06549ed62f3c0eeb8569ed210e99f6f08
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-03-23 21:51:42 +0900

    replaced

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,14 +1,17 @@
-$(function() {
-  $(".link_p").click(function () {
-    $(".middle__bottom__ul__li__box__js").slideDown();
-  })
-  
-  let tabs = $(".js__li");
-  function tabSwitch() {
-    $(".active").removeClass("active");
-    $(this).addClass("active");
-    const index = tabs.index(this);
-    $(".li__bottom").removeClass("show").eq(index).addClass("show");
-  }
-  tabs.hover(tabSwitch);
-});
+$(document).on('turbolinks:load', function () {
+  $(function() {
+    $(".link_p").click(function () {
+      $(".middle__bottom__ul__li__box__js").slideDown();
+    })
+    
+    let tabs = $(".js__li");
+    function tabSwitch() {
+      $(".active").removeClass("active");
+      $(this).addClass("active");
+      const index = tabs.index(this);
+      $(".li__bottom").removeClass("show").eq(index).addClass("show");
+    }
+    tabs.hover(tabSwitch);
+  });
+})
+
