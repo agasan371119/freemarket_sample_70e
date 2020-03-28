3339e1ed41671397d07318855749f1421a29c557
agasan371119 <yna77351unichi@icloud.com>
2020-03-28 19:01:38 +0900

    add

--- app/assets/javascripts/items/new_item.js
+++ app/assets/javascripts/items/new_item.js
@@ -1,7 +1,5 @@
 $(document).on('turbolinks:load', function(){
-  console.log("aaa")
   $(function(){
-console.log("bb")
     //プレビューのhtmlを定義
     function buildHTML(count) {
       var html = `<div class="preview-box" id="preview-box__${count}">
