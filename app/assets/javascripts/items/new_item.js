f9960bf330f828834bfa8d3216ad7368fdab89a7
agasan371119 <yna77351unichi@icloud.com>
2020-03-29 14:47:06 +0900

    add

--- app/assets/javascripts/items/new_item.js
+++ app/assets/javascripts/items/new_item.js
@@ -81,8 +81,8 @@ $(document).on('turbolinks:load', function(){
         }
 
         //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=============
-        if ($(`#item_images_attributes_${id}__destroy`)){
-          $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
+        if ($(`#item_item_images_attributes_${id}__destroy`)){
+          $(`#item_item_images_attributes_${id}__destroy`).prop('checked',false);
         } 
         //=============================================================================
 
@@ -106,9 +106,9 @@ $(document).on('turbolinks:load', function(){
 
       //新規投稿時
       //削除用チェックボックスの有無で判定
-      if ($(`#item_images_attributes_${id}__destroy`).length == 0) {
+      if ($(`#item_item_images_attributes_${id}__destroy`).length == 0) {
         //フォームの中身を削除 
-        $(`#item_images_attributes_${id}_image`).val("");
+        $(`#item_item_images_attributes_${id}_image`).val("");
         var count = $('.preview-box').length;
         //5個めが消されたらラベルを表示
         if (count == 4) {
@@ -122,7 +122,7 @@ $(document).on('turbolinks:load', function(){
       } else {
 
         //投稿編集時
-        $(`#item_images_attributes_${id}__destroy`).prop('checked',true);
+        $(`#item_item_images_attributes_${id}__destroy`).prop('checked',true);
         //5個めが消されたらラベルを表示
         if (count == 4) {
           $('.label-content').show();
